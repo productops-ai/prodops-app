6345caa06ced91fa8ab5e51247cbada04ac3c8c2
Sai Shashank Gaddam <saishashankgaddam@Sais-Air.lan>
2024-10-19 17:46:20 -0500

    feat(github-actions): Added CI/CD deployment for ProdOps backend

--- prodops-ai-backend/app.py
+++ prodops-ai-backend/app.py
@@ -1,6 +1,10 @@
 from app import create_app
+from routes_vllm import main
 
 app = create_app()
+
+# Register the blueprint
+app.register_blueprint(main)
 
 if __name__ == '__main__':
     app.run(debug=True)
